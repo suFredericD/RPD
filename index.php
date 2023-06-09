03d6005414bb298710d6d54cecce10ed069c0759
suFredericD <ilric6666@gmail.com>
2023-06-09 05:43:43 +0200

    On branch master
    Initial commit
    
    1. Setting up the repo
    2. Adding index.php : home page
    3. Adding main.css : main stylesheet
    4. Adding media/icons/cropped-RealToken_Favicon-192x192.png : favicon
    5. Adding media/icons/cropped-RealToken_Favicon-32x32.png : favicon
    
    Changes to be committed:
    	new file:   index.php
    	new file:   main.css
    	new file:   media/icons/cropped-RealToken_Favicon-192x192.png
    	new file:   media/icons/cropped-RealToken_Favicon-32x32.png
    	new file:   media/logos/RealT_Logo.svg

---
+++ index.php
@@ -0,0 +1,51 @@
+<!--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    Project     : RealT personal dashboard (RPD)
+    Purpose     : build an advanced personal dashboard for RealT tokens management
+    File        : index.php
+    Description : home page of the dashboard
+    Creation    : 2023-06-08
+    Author      : CoinMachine
+    Last update : 2023-06-08
+ --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --->
+ <?php
+ 
+ ?>
+ <!--- --- --- --- --- --- --- --- RealT Personal Dashboard --- --- --- --- --- --- --- --- --- --- --- --->
+ <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+    <meta charset="UTF-8">
+    <title>RPD | RealT personal dashboard</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="RealT personal dashboard (RPD): advanced personal dashboard for RealT tokens management">
+    <meta name="keywords" content="RealT, blockchain, smart contract, smart contracts, web3, wallet, token, investment, portfolio, dashboard, real estate">
+    <meta name="author" content="CoinMachine">
+    <favicon href="atom_Hero53.ico" />
+<!-- CSS parameters and animations script -->
+    <link rel="stylesheet" type="text/css" href="main.css">
+    <link href="https://fonts.googleapis.com/css?family=Fira+Sans+Condensed|Source+Sans+Pro&display=swap" rel="stylesheet">
+</head>
+<body>
+<!-- Modal header -->
+    <section class="modal-header">
+    <img src="media/logos/RealT_Logo.svg" alt="modal-icon" class="modal-icon">
+        <h1>RealT Personal Dashboard</h1>
+    </section>
+<!-- Modal header -->
+    <section id="content-container">
+        <nav id="main-nav">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="portfolio.php">Portfolio</a></li>
+
+
+            </ul>
+        </nav>
+
+
+
+
+
+    </section>
+<!-- JavaScript animations and DOM manipulations integrated script -->
+    <script src="index.js"></script>
+</body>
+</html>
